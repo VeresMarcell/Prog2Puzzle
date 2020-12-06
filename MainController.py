efc13e77ef430a6a20323c901ee8f7b4e8eb2ff7
vmarc <veresmarcell0727@gmail.com>
2020-12-06 13:21:50 +0100

    Képvágás módszerére rájöttem, kommentelt magyarázat

--- MainController.py
+++ MainController.py
@@ -18,23 +18,23 @@ class Controller:
 
         self.mw.show()
 
-    def clickImgBtn(self):
+    def clickImgBtn(self):                  # Kép betöltésére használt gomb metódusa
         dialog = QFileDialog.getOpenFileName(None, "Open Image", '', "Image Files (*.png *.jpg *.bmp)")
-        self.ui.imgPathLineEdit.setText(dialog[0])
-        self.ui.imgPathLineEdit.setReadOnly(True)
-        pix = QPixmap(dialog[0])
-        pix = pix.scaled(630, 630, QtCore.Qt.KeepAspectRatio)
-        pix = pix.copy(0,0,200,200)
-        item = QtWidgets.QGraphicsPixmapItem(pix)
+        self.ui.imgPathLineEdit.setText(dialog[0])          # Line Edit, amiben láthatjuk a betöltött fotó helyét,
+        self.ui.imgPathLineEdit.setReadOnly(True)           # de nem írhatjuk át. Visszajelzés a sikeres betöltésre
+        pix = QPixmap(dialog[0])                                # innen a metódus végéig a kép pixmapként való betöltése
+        pix = pix.scaled(630, 630, QtCore.Qt.KeepAspectRatio)   # át méretezése
+        # pix = pix.copy(0,0,200,200)                             # Ez a sor csak teszt hogy hogyan tudok körbevágni egy képrészt
+        item = QtWidgets.QGraphicsPixmapItem(pix)               # majd a GraphicView GraphicScene-é való állítása
         scene = QtWidgets.QGraphicsScene()
         scene.addItem(item)
-        self.ui.imgView.resize(630,630)
+        # self.ui.imgView.resize(630,630)       # Ez a parancs feleslegesnek tűnik de félek kitörölni, nehogy elfelejtsem
         self.ui.imgView.setScene(scene)
 
     def clickNgBtn(self):
         pass
 
-class Tiles():
+class Tiles():          # Félkész class amiben valahogy majd meg akarom oldani a kép kockákra vágását
 
     def __init__(self):
         self.tiles = []
