40ee770098a6e85c9e83f7decb6f436fe95f9deb
vmarc <veresmarcell0727@gmail.com>
2020-12-08 19:02:03 +0100

    QtCore.Qt.KeepAspectRatio kitörlése hogy ne legyenek fehér zónák

--- MainController.py
+++ MainController.py
@@ -22,13 +22,14 @@ class Controller:
         self.ui.imgPathLineEdit.setText(dialog[0])          # Line Edit, amiben láthatjuk a betöltött fotó helyét,
         self.ui.imgPathLineEdit.setReadOnly(True)           # de nem írhatjuk át. Visszajelzés a sikeres betöltésre
         pix = QPixmap(dialog[0])                                # innen a metódus végéig a kép pixmapként való betöltése
-        pix = pix.scaled(630, 630, QtCore.Qt.KeepAspectRatio)   # át méretezése
+        pix = pix.scaled(630, 630)   # át méretezése
         # pix = pix.copy(0,0,200,200)                             # Ez a sor csak teszt hogy hogyan tudok körbevágni egy képrészt
         item = QtWidgets.QGraphicsPixmapItem(pix)               # majd a GraphicView GraphicScene-é való állítása
         scene = QtWidgets.QGraphicsScene()
         scene.addItem(item)
         # self.ui.imgView.resize(630,630)       # Ez a parancs feleslegesnek tűnik de félek kitörölni, nehogy elfelejtsem
         self.ui.imgView.setScene(scene)
+        print(pix.size())
 
     def clickNgBtn(self):
         pass
