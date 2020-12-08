64222fb1612385cb34a4da40cca424cb764b4f57
vmarc <veresmarcell0727@gmail.com>
2020-12-08 23:17:52 +0100

    Difficulty darabolás, Give Up button, időmérés

--- MainController.py
+++ MainController.py
@@ -3,8 +3,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QFileDialog
 from MainWin import Ui_MainMenu
 from PyQt5.QtGui import QPixmap
-import random
-
+from timeit import default_timer as timer
 
 class Controller:
 
@@ -14,6 +13,7 @@ class Controller:
         self.ui.setupUi(self.mw)
         self.ui.ImgBtn.clicked.connect(self.clickImgBtn)
         self.ui.NgBtn.clicked.connect(self.clickNgBtn)
+        self.ui.GiveUpBtn.clicked.connect(self.clickGiveUpButton)
         self.imPath = ''
         self.solution = []
 
@@ -98,6 +98,7 @@ class Controller:
         diff = self.ui.DiffBox.currentText()
         darabok.createTiles(self.imPath, diff)
         TilesLs = darabok.getTiles()
+        Controller.start = timer()
 
         x = 0
         y = 0
@@ -138,6 +139,67 @@ class Controller:
                 if x == 9:
                     break
 
+    def clickGiveUpButton(self):
+
+        Controller.end = timer()
+        Tneeded = Controller.end-Controller.start
+        Tneeded = '{time:.2f}'.format(time=Tneeded)
+        QtWidgets.QMessageBox.information(None, "You gave up",
+                                            'You were trying for {} seconds'.format(Tneeded),
+                                            QtWidgets.QMessageBox.Ok)
+
+        pix = QPixmap(self.imPath)
+        pix = pix.scaled(630, 630)
+        scene = QtWidgets.QGraphicsScene()
+        self.ui.imgView.setScene(scene)
+        self.ui.imgView.setSceneRect(0, 0, 630, 630)
+        self.ui.imgView.setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
+        darabok = Tiles()
+        darabok.createTiles(self.imPath, self.ui.DiffBox.currentText())
+        TilesLs = darabok.getTiles()
+        diff = self.ui.DiffBox.currentText()
+
+        x = 0
+        y = 0
+
+        if diff == 'Easy':
+            for i in TilesLs:
+                item = QtWidgets.QGraphicsPixmapItem(i)
+                scene.addItem(item)
+                item.setPos(x * (pix.size().height() / 5), y * (pix.size().width() / 5))
+                self.solution.append(item.pos())
+                y += 1
+                if y == 5:
+                    x += 1
+                    y = 0
+                if x == 5:
+                    break
+
+        if diff == 'Normal':
+            for i in TilesLs:
+                item = QtWidgets.QGraphicsPixmapItem(i)
+                scene.addItem(item)
+                item.setPos(x * (pix.size().height() / 7), y * (pix.size().width() / 7))
+                self.solution.append(item.pos())
+                y += 1
+                if y == 7:
+                    x += 1
+                    y = 0
+                if x == 7:
+                    break
+
+        if diff == 'Hard':
+            for i in TilesLs:
+                item = QtWidgets.QGraphicsPixmapItem(i)
+                scene.addItem(item)
+                item.setPos(x * (pix.size().height() / 9), y * (pix.size().width() / 9))
+                self.solution.append(item.pos())
+                y += 1
+                if y == 9:
+                    x += 1
+                    y = 0
+                if x == 9:
+                    break
 
 
 class Tiles():          # Félkész class amiben valahogy majd meg akarom oldani a kép kockákra vágását
@@ -170,7 +232,6 @@ class Tiles():          # Félkész class amiben valahogy majd meg akarom oldani
 
     def getTiles(self):
         return self.tiles
-
 
 
 #MAIN
